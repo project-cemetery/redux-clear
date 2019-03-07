924ed45918837b2e1352200fb0799050bc86a573
Igor Kamyshev <igor@kamyshev.me>
2019-03-07 12:13:06 +0200

    fix(fetching): add miss exports for fetching

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,7 @@ export { ClearAction } from './ClearAction'
 
 export { createClearReduxWithFetching } from './createClearReduxWithFetching'
 export { WithFetchingState } from './WithFetchingState'
+
+export { createFetchingRedux } from './fetching-redux/createFetchingRedux'
+export { FethcingActions } from './fetching-redux/FetchingActions'
+export { FetchingState } from './fetching-redux/FetchingState'
